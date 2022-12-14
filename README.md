b4f964c708ab5bd96077e0cccd7275d34b1dcbcc
michaela <michaelaro94@gmail.com>
2022-12-13 20:44:05 -0500

    fixed unstable reference bug for fetchBooks function

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Create, edit & delete books from your reading list.
 This app was created along side a Modern React with Redux course hosted on udemy.com 
 
 ## Concepts learned/demostrated
--Building with reusable components
--React State system
--Using an API with React
--Handing forms
--Data persistence with API requests
-    -The Effect hook
--Communication using Context system
+- Building with reusable components
+- React State system
+- Using an API with React
+- Handing forms
+- Data persistence with API requests
+    - The Effect hook
+- Communication using Context system
 
 ## Start the application and local api server
 `npm run` and `npm run server`
--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react';
+import {createContext, useState, useCallback} from 'react';
 import axios from 'axios';
 
 const BooksContext = createContext();
@@ -6,10 +6,12 @@ const BooksContext = createContext();
 function Provider({children}){
     const [books, setBooks] = useState([]);
 
-    const fetchBooks = async () => {
+    //useCallback passes function from first render (unless passed some elements that have changed)
+    //keeps useEffect from re-running when passed function in array (creates loop)
+    const fetchBooks = useCallback(async () => {
         const response = await axios.get('http://localhost:3001/books');
         setBooks(response.data);
-    };
+    }, []);
 
     const editBookById = async (id, newTitle) => {
         const response = await axios.put('http://localhost:3001/books/' + id, {
